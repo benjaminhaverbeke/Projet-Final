a5d7cff6435b98eafa4fe459c6de077dee875914
benjamin.haverbeke <benjamin.haverbeke@outlook.com>
2024-02-29 15:33:33 +0000

    footer

--- Ben/script.js
+++ Ben/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     
     let button = document.querySelector('.button');
+    let header = document.querySelector('header');
     console.log(button);
     button.addEventListener("click", function(){
-        
+        header.scrolltop = 20;
     })
     
 });
