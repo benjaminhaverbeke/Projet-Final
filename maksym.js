787d41fd6d2dd55532b327a9e02da9ca724900af
VOZNICHKA Maksym <maksym.voznicka@gmail.com>
2024-02-29 18:59:16 +0100

    some changes in js maksym

--- maksym.js
+++ maksym.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   
-  (() => {
     const burgerOpen = document.querySelector('.header__burger')
     const burgerClose = document.querySelector('.header__nav__close')
     const navigation = document.querySelector('.header__nav')
   
-    burgerOpen.addEventListener('click', () => {
+    burgerOpen.addEventListener('click', function() {
       navigation.classList.add('active')
     })
   
-    burgerClose.addEventListener('click', () => {
+    burgerClose.addEventListener('click', function() {
       navigation.classList.remove('active')
     })
-  })();
 });
