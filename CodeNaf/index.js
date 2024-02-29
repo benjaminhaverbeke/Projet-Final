8becf7ac00742c327dc97abeeeddad730f87d6d2
Antoy Nafion <antoynafion@gmail.com>
2024-02-29 17:27:33 +0100

    stylisation des div: Centrage, couleur et border. Réorganisation de mon fichier HTML.

--- CodeNaf/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const slider = new A11YSlider(document.querySelector(".slider"));
-    console.log(slider);
-    console.log("lol")
-
-})
