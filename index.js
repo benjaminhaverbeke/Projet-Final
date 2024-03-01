51042fa85cf5657473b8f6dae0d29c79c2e85a90
benjamin.haverbeke <benjamin.haverbeke@outlook.com>
2024-03-01 09:30:48 +0000

    footer4

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
-    const burgerOpen = document.querySelector('.header__burger')
-    const burgerClose = document.querySelector('.header__nav__close')
-    const navigation = document.querySelector('.header__nav')
-  
-    burgerOpen.addEventListener('click', function() {
-      navigation.classList.add('active')
-    })
-  
-    burgerClose.addEventListener('click', function() {
-      navigation.classList.remove('active')
-    })
+
+  const burgerOpen = document.querySelector('.header__burger')
+  const burgerClose = document.querySelector('.header__nav__close')
+  const navigation = document.querySelector('.header__nav')
+
+  burgerOpen.addEventListener('click', function() {
+    navigation.classList.add('active')
+  })
+
+  burgerClose.addEventListener('click', function() {
+    navigation.classList.remove('active')
+  })
+
+  let button = document.querySelector('.button');
+
+  console.log(button);
+  button.addEventListener("click", function() {
+    console.log(event);
+
+    window.scroll(0, 0);
+
+
+  });
+
+
+
 });
